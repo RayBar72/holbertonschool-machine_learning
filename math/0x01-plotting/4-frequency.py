736269be4618b1697fac69da341fef48459d5e30
RayBar72 <3963@holbertonschool.com>
2022-08-01 12:51:24 -0300

    Up to task 4

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Function that plot a histogram of student scores for a project:
+    The x-axis should be labeled Grades
+    The y-axis should be labeled Number of Students
+    The x-axis should have bins every 10 units
+    The title should be Project A
+    The bars should be outlined in black
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.hist(student_grades, bins=range(10, 101, 10), edgecolor='black')
+plt.xlabel('Grades')
+plt.xlim(0,100)
+plt.xticks(range(10, 101, 10))
+plt.ylabel('Number of Students')
+plt.ylim(0, 30)
+plt.title('Project A')
+plt.show()
