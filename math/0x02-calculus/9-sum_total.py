c9fe2206f5c383376449ad8f4c5d1e0a62a8240d
RayBar72 <3963@holbertonschool.com>
2022-08-02 15:31:06 -0300

    Task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,6 +6,21 @@ Function that calculates the sumatory from:
     The function is i ** 2
 '''
 
+
+def sumation_recursive(ls, n):
+    '''Function that calculates the sumatory recursive'''
+    if n:
+        i = n
+        i = i ** 2
+        ls.append(i)
+        # print(ls)
+        sumation_recursive(ls, n - 1)
+    return ls
+
+
 def summation_i_squared(n):
     '''Function that calculates sumatory'''
-    
+    if n < 1 or type(n) is not int or n is None:
+        return None
+    ls = []
+    return sum(sumation_recursive(ls, n))
