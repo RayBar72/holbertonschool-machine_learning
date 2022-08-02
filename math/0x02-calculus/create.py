b5fdf921eb892ee9c5db3b958ceb77721e66f273
RayBar72 <3963@holbertonschool.com>
2022-08-02 07:06:59 -0300

    Backup 220802h0706

---
+++ math/0x02-calculus/create.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''Function that creates the files for exercises'''
+
+
+lsfiles = [
+    '0-sigma_is_for_sum',
+    '1-seegma',
+    '2-pi_is_for_product',
+    '3-pee',
+    '4-hello_derivatives,'
+    '5-log_on_fire',
+    '6-voltaire',
+    '7-partial_truths',
+    '8-all-together',
+    '9-main.py',
+    '9-sum_total.py',
+    '10-main.py',
+    '10-matisse.py',
+    '11-integral',
+    '12-integral',
+    '13-definite',
+    '14-definite',
+    '15-definite',
+    '16-double',
+    '17-main.py'
+    '17-integrate.py'
+]
+
+for i in lsfiles:
+    with open(i, 'w') as f:
+        pass
