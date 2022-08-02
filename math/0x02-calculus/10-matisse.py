a4e97fc7567e69b6e83e20684b397a1a1abbd4b4
RayBar72 <3963@holbertonschool.com>
2022-08-02 15:44:13 -0300

    Task 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Function that casculates polynomial derivatives'''
+
+
+def poly_derivative(poly):
+    '''Function that casculates polynomial derivatives'''
+    if type(poly) != list:
+        return None
+    ls = []
+    largo = len(poly)
+    if largo == 1:
+        return ls.append(0)
+    for i in range(largo):
+        if i == 0:
+            pass
+        else:
+            ls.append(poly[i] * i)
+    return ls
