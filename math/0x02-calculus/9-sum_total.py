22f6809d2ec8a4e5d78d1cd72fa6d82f70824775
RayBar72 <3963@holbertonschool.com>
2022-08-02 12:00:08 -0300

    Backup 220802h1200

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,24 +6,6 @@ Function that calculates the sumatory from:
     The function is i ** 2
 '''
 
-def rec_summation(n, end, steps, i =1, y = 1):
-    if end == n:
-        y += y
-        print('y={}, i={}'.format(y, i))
-        return y
-    else:
-        print('y={}, i={}'.format(y, i))
-        rec_summation(n, end + steps, steps, i = i + steps, y = i ** 2)
-
-
 def summation_i_squared(n):
     '''Function that calculates sumatory'''
-    if type(n) is not int:
-        return None
-    if n <= 0:
-        steps = -1
-    else:
-        steps = 1
-    end = 0
-    print('n = {}, end = {}, steps ={}'.format(n, end, steps))
-    return rec_summation(n, end, steps)
+    
