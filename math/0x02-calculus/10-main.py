59aa46adb2e7a0335b3a47c83655bd63e49d6c64
RayBar72 <3963@holbertonschool.com>
2022-08-03 19:42:23 -0300

    Making fixes v3

--- math/0x02-calculus/10-main.py
+++ math/0x02-calculus/10-main.py
@@ -2,5 +2,26 @@
 
 poly_derivative = __import__('10-matisse').poly_derivative
 
-poly = ['x', 'y']
+# No list
+poly = 'xyz'
 print(poly_derivative(poly))
+
+# No poly
+poly = []
+print(poly_derivative(poly))
+
+# None in list
+poly = [1, 2, 3, 4, None]
+print(poly_derivative(poly))
+
+# Largo 1
+poly = [1]
+print(poly_derivative(poly))
+
+# con letras
+poly = ['xyz']
+print(poly_derivative(poly))
+
+
+
+
--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -14,12 +14,14 @@ def poly_derivative(poly):
         return None
     elif largo == 0:
         return None
-    elif largo == 1:
-        return [0]
+    # elif largo == 1:
+    #     return [0]
     else:
         for i in range(largo):
             if type(poly[i]) is int or type(poly[i]) is float:
-                if i == 0:
+                if largo == 1:
+                    return [0]
+                elif i == 0:
                     pass
                 else:
                     ls.append(poly[i] * i)
