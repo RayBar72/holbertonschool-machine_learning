0f465b3902340514d555a5dd4338bc963fbb122c
RayBar72 <3963@holbertonschool.com>
2022-08-04 16:06:20 -0300

    Reorganizing task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,13 +4,15 @@
 
 def poly_derivative(poly):
     '''Function that caculates polynomial derivatives'''
-    aux = []
-    if (type(poly) is not list) or (len(poly) == 0):
+    ls = []
+    if type(poly) is not list:
         return None
-    if (not all(isinstance(n, int) or isinstance(n, float) for n in poly)):
+    if len(poly) == 0:
+        return None
+    if (not all(isinstance(x, (int, float)) for x in poly)):
         return None
     for i in range(1, len(poly)):
-        aux.append(i * poly[i])
-    if all(a == 0 for a in aux):
+        ls.append(poly[i] * i)
+    if all(x == 0 for x in ls):
         return [0]
-    return aux
+    return ls
