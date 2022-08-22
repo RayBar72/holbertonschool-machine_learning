4823c39636a31abf40a766a043ccd2cf00a8a21f
RayBar72 <3963@holbertonschool.com>
2022-08-22 19:43:09 -0300

    Backup 220822h1943

--- supervised_learning/0x01-classification/7-neuron.py
+++ supervised_learning/0x01-classification/7-neuron.py
@@ -84,10 +84,10 @@ class Neuron():
             raise TypeError('alpha must be a float')
         if alpha < 0:
             raise ValueError('alpha must be positive')
-        if verbose is not True or graph is not True:
+        if verbose is True or graph is True:
             if not isinstance(step, int):
                 raise TypeError('step must be an integer')
-            if step < 0:
+            if step < 0 or step > iterations:
                 raise ValueError('step must be positive and <= iterations')
         iteration = []
         c = []
