d7bbef3d83ad8aa7fe257fd75ae550b971069471
RayBar72 <3963@holbertonschool.com>
2022-08-23 12:28:59 -0300

    Backup 220823h1228

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -19,18 +19,20 @@ class DeepNeuralNetwork():
         if len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
         self.nx = nx
-        self.layers = layers
         self.L = len(layers)
         self.cache = {}
-        self.weights = {}
-        for x in range(self.L):
-            if layers[x] < 1 or not isinstance(layers[x], int):
+        weights = {}
+        for x in range(len(layers)):
+            if layers[x] < 1:
                 raise TypeError('layers must be a list of positive integers')
-            self.weights['b' + str(x + 1)] = np.zeros([layers[x], 1])
+            if not isinstance(layers[x], int):
+                raise TypeError('layers must be a list of positive integers')
+            weights['b' + str(x + 1)] = np.zeros([layers[x], 1])
             if x == 0:
                 He_et0 = np.random.randn(layers[x], nx) * np.sqrt(2 / nx)
-                self.weights['W' + str(x + 1)] = He_et0
+                weights['W' + str(x + 1)] = He_et0
             if x > 0:
                 He_et_p1 = np.random.rand(layers[x], layers[x - 1])
                 He_et_p2 = np.sqrt(2 / layers[x - 1])
-                self.weights["W" + str(x + 1)] = He_et_p1 * He_et_p2
+                weights['W' + str(x + 1)] = He_et_p1 * He_et_p2
+        self.weights = weights
