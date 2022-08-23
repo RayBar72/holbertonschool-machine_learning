a1070cfd3f3b78f423bd689521ae1f0e504cf00e
RayBar72 <3963@holbertonschool.com>
2022-08-23 17:17:29 -0300

    Backup 220823h1717

--- supervised_learning/0x01-classification/26-deep_neural_network.py
+++ supervised_learning/0x01-classification/26-deep_neural_network.py
@@ -154,8 +154,8 @@ class DeepNeuralNetwork():
         '''
         Saves the instance object to a file in pickle format
         '''
-        if filename[-4:] != ".pkl":
-            filename += ".pkl"
+        # if filename[-4:] != ".pkl":
+        #     filename += ".pkl"
         with open(filename, "wb") as f:
             pickle.dump(self, f)
 
