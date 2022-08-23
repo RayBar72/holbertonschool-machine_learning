fac1b72632545d091a54dac90e50df6f5b95218e
RayBar72 <3963@holbertonschool.com>
2022-08-23 12:41:56 -0300

    Backup 220823h1241

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -11,27 +11,25 @@ class DeepNeuralNetwork():
     def __init__(self, nx, layers):
         '''Class constructur'''
         if type(nx) is not int:
-            raise TypeError('nx must be an integer')
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
+            raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
-            raise TypeError('layers must be a list of positive integers')
+            raise TypeError("layers must be a list of positive integers")
         if len(layers) == 0:
-            raise TypeError('layers must be a list of positive integers')
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
-        weights = {}
+        self.weights = {}
         for x in range(self.L):
-            if type(layers[x]) is not int:
-                raise TypeError('layers must be a list of positive integers')
-            if layers[x] < 1:
-                raise TypeError('layers must be a list of positive integers')
-            weights['b' + str(x + 1)] = np.zeros([layers[x], 1])
+            if layers[x] < 1 or type(layers[x]) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights["b" + str(x + 1)] = np.zeros((layers[x], 1))
             if x == 0:
-                He_et0 = np.random.randn(layers[x], nx) * np.sqrt(2 / nx)
-                weights['W' + str(x + 1)] = He_et0
+                He_et_0 = np.random.randn(layers[x], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str(x + 1)] = He_et_0
             if x > 0:
-                He_et_p1 = np.random.rand(layers[x], layers[x - 1])
-                He_et_p2 = np.sqrt(2 / layers[x - 1])
-                weights['W' + str(x + 1)] = He_et_p1 * He_et_p2
-        self.weights = weights
+                He_et_1 = np.random.randn(layers[x], layers[x - 1])
+                He_et_2 = np.sqrt(2 / layers[x - 1])
+                He_et_3 = He_et_1 * He_et_2
+                self.weights["W" + str(x + 1)] = He_et_3
