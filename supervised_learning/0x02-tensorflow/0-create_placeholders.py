1675b9958df55c5c8f2a305e9f612f7ee419bc2c
RayBar72 <3963@holbertonschool.com>
2022-08-25 09:59:37 -0300

    Backup 220825h0959

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -3,3 +3,25 @@
 Modulos that contains a function that
 returns two placeholders, x and y
 '''
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    '''
+    Function that returns two place holders, x an y
+
+    Parameters
+    ----------
+    nx : int
+        Number of feature columns in our data.
+    classes : int
+        Number of classes in our classifier.
+
+    Returns
+    -------
+    Placeholders x and y.
+
+    '''
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+Modulo that creates layers with tensor flow
+'''
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    '''
+    Function that creates layers for a NN
+
+    Parameters
+    ----------
+    prev : np.narray
+        Tensor output of the previous layer.
+    n : int
+        Number of nodes in the layer.
+    activation : Function
+        Activation function that the layer should use.
+
+    Returns: the tensor output of layer
+    -------
+    None.
+
+    '''
+    activa = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.layers.Dense(n, activation=activation,
+                              kernel_initializer= activa, name='layer')
+    return layer(prev)
