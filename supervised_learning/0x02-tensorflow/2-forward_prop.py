af0d4706aca84066f363c8715ead710f5cc8f351
RayBar72 <3963@holbertonschool.com>
2022-08-25 16:38:13 -0300

    Backup 220825h1637

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''
+Modulus that contains a functiont that contains the
+forward propagation graph for a neural network
+'''
+import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    '''
+    functiont that contains the forward propagation
+    graph for a neural network
+
+    Parameters
+    ----------
+    x : Placeholder
+        Input data.
+    layer_sizes : list
+        DESCRIPTION. Contains the number of nodes in each layer.
+    activations : list
+        DESCRIPTION. Contains the activation functions for each layer.
+
+    Returns: The prediction of network tensor form
+    '''
+    for i in range(len(layer_sizes)):
+        if i == 0:
+            estimation = create_layer(x, layer_sizes[i], activations[0])
+        else:
+            estimation = create_layer(estimation, layer_sizes[i],
+                                      activations[0])
+    return estimation
