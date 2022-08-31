e6bf542dec88c5cb03687f9c0fa2ed5340329a1b
RayBar72 <3963@holbertonschool.com>
2022-08-31 11:03:40 -0300

    Updates from jupyter

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -2,12 +2,14 @@
 '''
 Function that trains a loaded neural network
 '''
+import numpy as np
 import tensorflow.compat.v1 as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
-                     batch_size=32, epochs=5, load_path="/tmp/model.ckpt",
+                     batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.ckpt"):
     '''
     Train a mini-batch of training data
