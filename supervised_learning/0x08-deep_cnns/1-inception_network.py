aa33004f51dfac124ffa3d12fe9031ef1f23bf33
RayBar72 <3963@holbertonschool.com>
2022-09-27 13:45:36 -0300

    Backup 220927h1345

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -26,38 +26,43 @@ def inception_network():
     MP1 = K.layers.MaxPool2D(pool_size=(3, 3),
                              strides=2,
                              padding='same')(C1)
-    C2 = K.layers.Conv2D(192,
-                         kernel_size=(7, 7),
+    C2 = K.layers.Conv2D(64,
+                         kernel_size=(1, 1),
                          strides=1,
                          padding='same',
                          activation='relu')(MP1)
+    C3 = K.layers.Conv2D(192,
+                         kernel_size=(3, 3),
+                         strides=1,
+                         padding='same',
+                         activation='relu')(C2)
     MP2 = K.layers.MaxPool2D(pool_size=(3, 3),
                              strides=2,
-                             padding='same')(C2)
+                             padding='same')(C3)
     IN1A = inception_block(MP2,
                            [64, 96, 128, 16, 32, 32])
     IN1B = inception_block(IN1A,
-                           [64, 96, 128, 16, 32, 32])
+                           [128, 128, 192, 32, 96, 64])
     MP3 = K.layers.MaxPool2D(pool_size=(3, 3),
                              strides=2,
                              padding='same')(IN1B)
     IN2A = inception_block(MP3,
-                           [64, 96, 128, 16, 32, 32])
+                           [192, 96, 208, 16, 48, 64])
     IN2B = inception_block(IN2A,
-                           [64, 96, 128, 16, 32, 32])
+                           [160, 112, 224, 24, 64, 64])
     IN2C = inception_block(IN2B,
-                           [64, 96, 128, 16, 32, 32])
+                           [128, 128, 256, 24, 64, 64])
     IN2D = inception_block(IN2C,
-                           [64, 96, 128, 16, 32, 32])
+                           [112, 144, 288, 32, 64, 64])
     IN2E = inception_block(IN2D,
-                           [64, 96, 128, 16, 32, 32])
+                           [256, 160, 320, 32, 128, 128])
     MP4 = K.layers.MaxPool2D(pool_size=(3, 3),
                              strides=2,
                              padding='same')(IN2E)
     IN3A = inception_block(MP4,
-                           [64, 96, 128, 16, 32, 32])
+                           [256, 160, 320, 32, 128, 128])
     IN3B = inception_block(IN3A,
-                           [64, 96, 128, 16, 32, 32])
+                           [384, 192, 384, 48, 128, 128])
     AP1 = K.layers.AveragePooling2D(pool_size=(7, 7),
                                     strides=1,
                                     padding='same')(IN3B)
--- supervised_learning/0x08-deep_cnns/1-main.py
+++ supervised_learning/0x08-deep_cnns/1-main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
 import tensorflow.keras as K
+
+
 inception_network = __import__('1-inception_network').inception_network
 
 if __name__ == '__main__':
