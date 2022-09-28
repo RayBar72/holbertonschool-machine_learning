f7b891a85bdaba8ece3f3bc37042762844929da8
RayBar72 <3963@holbertonschool.com>
2022-09-28 18:02:01 -0300

    Backup 220928h1801

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -27,21 +27,21 @@ def identity_block(A_prev, filters):
     C0 = K.layers.Conv2D(F11,
                          kernel_size=(1, 1),
                          padding='same',
-                         activation='relu',
+                         # activation='relu',
                          kernel_initializer='he_normal')(A_prev)
     BN0 = K.layers.BatchNormalization(axis=3)(C0)
     R0 = K.layers.Activation('relu')(BN0)
     C1 = K.layers.Conv2D(F3,
                          kernel_size=(3, 3),
                          padding='same',
-                         activation='relu',
+                         # activation='relu',
                          kernel_initializer='he_normal')(R0)
     BN1 = K.layers.BatchNormalization(axis=3)(C1)
     R1 = K.layers.Activation('relu')(BN1)
     C2 = K.layers.Conv2D(F12,
                          kernel_size=(1, 1),
                          padding='same',
-                         activation='relu',
+                         # activation='relu',
                          kernel_initializer='he_normal')(R1)
     BN2 = K.layers.BatchNormalization(axis=3)(C2)
     AD = K.layers.Add()([BN2, A_prev])
