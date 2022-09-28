723379da982389a081134f08f940dc5940370137
RayBar72 <3963@holbertonschool.com>
2022-09-28 17:26:25 -0300

    Backup 220928h1526

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -24,40 +24,42 @@ def inception_block(A_prev, filters):
 
     '''
     F1, F3R, F3, F5R, F5, FPP = filters
+
     C1 = K.layers.Conv2D(F1,
                          kernel_size=(1, 1),
                          padding='same',
                          kernel_initializer='he_normal',
                          activation='relu')(A_prev)
+
     C2 = K.layers.Conv2D(F3R,
                          kernel_size=(1, 1),
                          padding='same',
                          kernel_initializer='he_normal',
                          activation='relu')(A_prev)
-    C3 = K.layers.Conv2D(F5R, kernel_size=(1, 1),
-                         padding='same',
-                         kernel_initializer='he_normal',
-                         activation='relu')(A_prev)
-    P1 = K.layers.MaxPool2D(pool_size=(3, 3),
-                            strides=(1, 1),
-                            padding='same')(A_prev)
     CC22 = K.layers.Conv2D(F3,
                            kernel_size=(3, 3),
                            padding='same',
                            kernel_initializer='he_normal',
                            activation='relu')(C2)
+
+    C3 = K.layers.Conv2D(F5R, kernel_size=(1, 1),
+                         padding='same',
+                         kernel_initializer='he_normal',
+                         activation='relu')(A_prev)
     CC32 = K.layers.Conv2D(F5,
                            kernel_size=(5, 5),
                            padding='same',
                            kernel_initializer='he_normal',
                            activation='relu')(C3)
+
+    P1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                               strides=(1, 1),
+                               padding='same')(A_prev)
     PC12 = K.layers.Conv2D(FPP,
                            kernel_size=(1, 1),
                            padding='same',
                            kernel_initializer='he_normal',
                            activation='relu')(P1)
-    concha = K.layers.Concatenate(axis=3)([C1,
-                                           CC22,
-                                           CC32,
-                                           PC12])
+
+    concha = K.layers.Concatenate([C1, CC22, CC32, PC12], axis=3)
     return concha
