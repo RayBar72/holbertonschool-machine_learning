be8f1c23ed8bb4bf73a40b9207a1b8fd1c3d1b73
RayBar72 <3963@holbertonschool.com>
2022-09-28 18:10:49 -0300

    Backup 220928h1810

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -4,6 +4,8 @@ Modulus that builds the ResNet-50 architecture as described in Deep
 Residual Learning for Image Recognition (2015)
 '''
 import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
