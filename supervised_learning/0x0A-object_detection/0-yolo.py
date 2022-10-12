e8d36f9170b3d17fbbd9ef0e688809d9f2c335d5
RayBar72 <3963@holbertonschool.com>
2022-10-12 18:32:37 -0300

    Backup 221012h1832

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -7,4 +7,11 @@ import tensorflow.keras as K
 
 class Yolo():
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        self.model = 
+        self.model = K.models.load_model(model_path)
+        # class_names = []
+        with open(classes_path, 'r') as f:
+            class_names = f.read().split('\n')
+        self.class_names = class_names
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
