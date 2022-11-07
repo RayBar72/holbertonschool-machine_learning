2183bb9c60d43799218647a5b16a93db5eaa1278
RayBar72 <3963@holbertonschool.com>
2022-11-07 16:46:05 -0300

    Task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -28,3 +28,23 @@ class MultiNormal():
         X_m = data - self.mean
 
         self.cov = np.matmul(X_m, X_m.T) / (n - 1)
+
+    def pdf(self, x):
+        if type(x) is not np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
+
+        if len(x.shape) != 2 or x.shape[1] != 1:
+            raise ValueError('x must have the'
+                             ' shape ({d}, 1)'.format(x.shape[0]))
+
+        x_m = self.mean
+        sigma = self.cov
+        dim = x.shape[0]
+
+        expo_1 = np.matmul(np.linalg.inv(sigma), x - x_m)
+        expo_2 = (- 1 / 2) * np.matmul((x - x_m).T, expo_1)
+        expo = np.sum(np.exp(expo_2))
+        deto = np.sum(np.linalg.det(sigma) ** (- 1 / 2))
+        pillo = np.sum((2 * np.pi) ** (- dim / 2))
+
+        return pillo * deto * expo
