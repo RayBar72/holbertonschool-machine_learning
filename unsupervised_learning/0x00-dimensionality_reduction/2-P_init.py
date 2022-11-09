76a68ce714fc9cb353232771f9e98a515d7a8f66
RayBar72 <3963@holbertonschool.com>
2022-11-09 19:20:35 -0300

    Task 2

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Modulus that initializes variables for t-SNE'''
+import numpy as np
+
+
+def P_init(X, perplexity):
+    '''Function that calculates the P affinities in t-SNE'''
+    n, d = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    P = np.zeros(shape=[n, n])
+    betas = np.ones(shape=[n, 1])
+    H = np.log2(perplexity)
+    return D, P, betas, H
