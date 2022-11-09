43688b60190d7c56d2c3e5b3e5e4cbb399f8f2d9
RayBar72 <3963@holbertonschool.com>
2022-11-09 19:28:58 -0300

    Task 2

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -8,6 +8,7 @@ def P_init(X, perplexity):
     n, d = X.shape
     sum_X = np.sum(np.square(X), 1)
     D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    np.fill_diagonal(D, 0.0)
     P = np.zeros(shape=[n, n])
     betas = np.ones(shape=[n, 1])
     H = np.log2(perplexity)
