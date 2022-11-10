fa287f7b8df9c7cfca6691c4c8dd43a724d3c8d1
RayBar72 <3963@holbertonschool.com>
2022-11-10 15:05:55 -0300

    Task 0

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def likelihood(x, n, P):
     '''Function that calculates likelihoo'''
-    # if not isinstance(n, int) or (n <= 0):
-    #     raise ValueError('n must be a positive integer')
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError('n must be a positive integer')
 
     # if not isinstance(x, int) or (x < 0):
     #     raise ValueError('x must be an integer \
