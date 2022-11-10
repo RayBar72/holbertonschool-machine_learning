ada9dfa93d59082e3d9290d70b2a34229b18d282
RayBar72 <3963@holbertonschool.com>
2022-11-10 15:54:09 -0300

    Task 2

---
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''Modulus that calculates the marginal probability'''
+import numpy as np
+inter = __import__('1-intersection').intersection
+
+
+def marginal(x, n, P, Pr):
+    '''Function that calculates marginal probability'''
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError('n must be a positive integer')
+
+    if not isinstance(x, int) or (x < 0):
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError('All values in P must be in the range [0, 1]')
+
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
+
+    return np.sum(inter(x, n, P, Pr))
