00c418ddd6f27458c459dbcffa3ece273ad47b41
RayBar72 <3963@holbertonschool.com>
2022-11-16 20:44:27 -0300

    Task 10

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -9,12 +9,6 @@ def kmeans(X, k):
     '''
     Function that performs K-means in a dataset
     '''
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None
-
-    if type(k) is not int or k < 1:
-        return None, None
-
     kmeans = sklearn.KMeans(k).fit(X)
 
     return kmeans.cluster_centers_, kmeans.labels_
