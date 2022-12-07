6aacc3df1f5f53ffa14864bfa7c1635a497bafdf
RayBar72 <3963@holbertonschool.com>
2022-12-07 14:05:16 +0000

    Adjusting L1

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -13,8 +13,10 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     inputs = keras.Input(shape=(input_dims,))
     x = inputs
     for l in hidden_layers:
-        x = keras.layers.Dense(l, 'relu', kernel_regularizer=l1)(x)
-    outputs = keras.layers.Dense(latent_dims, 'relu', kernel_regularizer=l1)(x)
+        x = keras.layers.Dense(l, 'relu')(x)
+    outputs = keras.layers.Dense(latent_dims,
+                                 'relu',
+                                 activity_regularizer=l1)(x)
     encoder = keras.Model(inputs, outputs)
 
     inputs2 = keras.Input(shape=(latent_dims,))
