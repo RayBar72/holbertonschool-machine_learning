4ece5eb992f1d23fe0c3a523f48fd8c14e5daa3d
RayBar72 <3963@holbertonschool.com>
2023-01-15 09:59:33 -0300

    Task 0

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -7,6 +7,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 def dict_vecto(palabras, token_pattern='(?u)\\b\\w+\\b', ngram_range=(1, 1)):
+    """_summary_
+
+    Args:
+        palabras (list): Function that creates a dictionary
+        token_pattern (str, optional): Regrex for tokenization. Defaults to '(?u)\b\w+\b'.
+        ngram_range (tuple, optional): ngram for tokens. Defaults to (1, 1).
+
+    Returns:
+        dict: Dictionary with the words and the number of times they appear
+    """
     vectorizer = CountVectorizer(token_pattern='(?u)\\b\\w+\\b',
                                  ngram_range=(1, 1))
     x = vectorizer.fit_transform(palabras)
@@ -17,6 +27,17 @@ def dict_vecto(palabras, token_pattern='(?u)\\b\\w+\\b', ngram_range=(1, 1)):
 
 
 def references_dict(references, Sentence):
+    """Function that creates a dictionary several times in fuction of the
+    references list
+
+    Args:
+        references (list): References for the sentence
+        Sentence (dict): dictionary with the words and
+        the number of times they appear
+
+    Returns:
+        dict: with the words and the max number of times they appear
+    """
     g_list = list(Sentence)
     g_list = {x: 0 for x in g_list}
     for ref in references:
@@ -28,6 +49,7 @@ def references_dict(references, Sentence):
 
 
 def BP_function(sentence, references):
+    """Function that calculates the brevity penalty"""
     c = len(sentence)
     r = len(references[np.argmin([abs(len(i) - c) for i in references])])
     if c > r:
@@ -38,6 +60,7 @@ def BP_function(sentence, references):
 
 
 def uni_bleu(references, sentence):
+    """Function that calculates the unigram BLEU score for a sentence"""
     Sentence = dict_vecto(sentence)
     References = references_dict(references, Sentence)
 
