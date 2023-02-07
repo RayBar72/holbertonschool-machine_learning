ab7cfc8c071a6cbc53648ca3b10a4723a2cfa743
RayBar72 <3963@holbertonschool.com>
2023-02-07 19:37:21 -0300

    Final

---
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""4-play"""
+
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    state = env.reset()
+    env.render()
+    for step in range(max_steps):
+        action = np.argmax(Q[state])
+        new_state, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            return reward
+        state = new_state
+
+    env.close()
