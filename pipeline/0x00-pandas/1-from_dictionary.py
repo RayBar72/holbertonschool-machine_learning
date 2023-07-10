ea8435c48bcea09a5d525fc1733343d96c56551e
RayBar72 <3963@holbertonschool.com>
2023-07-10 23:51:05 +0000

    Pycodestyle review

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 diccionario = {
     'First': pd.Series([0.0, 0.5, 1.0, 1.5], index=['A', 'B', 'C', 'D']),
-    'Second': pd.Series(['one', 'two', 'three', 'four'], index=['A', 'B', 'C', 'D'])
+    'Second': pd.Series(['one', 'two', 'three', 'four'],
+                        index=['A', 'B', 'C', 'D'])
 }
 frame = pd.DataFrame(diccionario)
 globals()['df'] = frame
--- pipeline/0x00-pandas/12-hierarchy.py
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -11,7 +11,7 @@ df1 = df1.loc['1417411980':'1417417981']
 df2 = df2.set_index('Timestamp')
 df2 = df2.loc['1417411980':'1417417981']
 
-df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'] )
+df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 df = df.reorder_levels([1, 0])
 df = df.sort_index()
 
