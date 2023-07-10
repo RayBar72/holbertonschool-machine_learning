508079abdcf27d0000b6a3aa85fed85ce8f4f3d8
RayBar72 <3963@holbertonschool.com>
2023-07-10 11:22:14 -0300

    Up to task 2

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 
 diccionario = {
-    'First': [0.0, 0.5, 1.0, 1.5],
-    'Second': ['one', 'two', 'three', 'four']
+    'First': pd.Series([0.0, 0.5, 1.0, 1.5], index=['A', 'B', 'C', 'D']),
+    'Second': pd.Series(['one', 'two', 'three', 'four'], index=['A', 'B', 'C', 'D'])
 }
-frame = pd.DataFrame(diccionario, index=['A', 'B', 'C', 'D'])
+frame = pd.DataFrame(diccionario)
 globals()['df'] = frame
