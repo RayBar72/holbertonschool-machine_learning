6cf32d944314825084184089f3e8c8c1bebae955
RayBar72 <3963@holbertonschool.com>
2023-07-24 17:41:38 -0300

    Task 30

--- pipeline/0x02-databases/30-all.py
+++ pipeline/0x02-databases/30-all.py
@@ -6,4 +6,12 @@ import pymongo
 
 
 def list_all(mongo_collection):
+    """Function that list all documents in a collection
+
+    Args:
+        mongo_collection (pymongo): Document in the collection
+
+    Returns:
+        list: with the documents
+    """
     return mongo_collection.find()
